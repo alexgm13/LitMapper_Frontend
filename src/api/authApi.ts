7bd8467f66cc152a61cddce36127fe18eb60f256
alexgm13 <alexguzmanmiyashiro@gmail.com>
2025-10-17 13:30:33 -0500

    Fix: Error en las rutas para el backend

--- src/api/authApi.ts
+++ src/api/authApi.ts
@@ -5,12 +5,12 @@ import { AuthResponse, LoginRequest, RegisterRequest } from "@/lib/models/auth"
 
 export async function loginUser(payload: LoginRequest): Promise<AuthResponse> {
   
-  const { data } = await instancia.post<AuthResponse>("/users/login", payload)
+  const { data } = await instancia.post<AuthResponse>("api/users/login", payload)
   console.log(data)
   return data
 }
 
 export async function registerUser(payload: RegisterRequest): Promise<AuthResponse> {
-  const { data } = await instancia.post<AuthResponse>("/users/registrar", payload)
+  const { data } = await instancia.post<AuthResponse>("api/users/registrar", payload)
   return data
 }
